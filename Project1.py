b948254cb38c12a0ba7398e973dd800f6cd44eea
Ross Dewberry <ross.dewberry@hotmail.com>
2019-04-22 13:48:22 -0700

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -4,10 +4,12 @@ import numpy as np
 #If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 #Find the sum of all the multiples of 3 or 5 below 1000.
 
-N = np.zeros(1000)
+#Creates an array of all the 5 multiplies
+N5 = np.arange(5,1001,5)
 
+#Creates an array of all the 3 multiplies
+N3 = np.arange(3,1001,3)
+
+#Sums the two arrays
+N = np.sum(N5)+np.sum(N3)
 print(N)
-def five(N):
-     for i in range(1,1000):
-         for j in range(1,1000):
-             if 
